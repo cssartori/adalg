abb754afb5e4a3788c6b2993c7641692f72e50d6
cssartori <carlosartori2@gmail.com>
2017-03-30 16:40:47 -0300

    Heap done. Graph started

--- dijkstra/nheap.cpp
+++ dijkstra/nheap.cpp
@@ -4,7 +4,7 @@
 
 using namespace std;
 
-
+//Create a new n-heap with an unique item (key, e)
 NHeap::NHeap(int n, int key, int e){
 	this->heap.push_back(key);
 	this->data.push_back(e);
@@ -12,95 +12,118 @@ NHeap::NHeap(int n, int key, int e){
 	this->n = n;
 }
 
+//Insert a new item in the heap (key, e)
 void NHeap::insert(int key, int e){
 	heap.push_back(key);
 	data.push_back(e);
 	pos_heap[e] = heap.size()-1;
 	heapify_up(e);
-	printf("added %i, %i at %i\n", key, e, pos_heap[e]);
 }
 
-void NHeap::update(int nkey, int e){
+//Update the key of an item e
+void NHeap::update_key(int nkey, int e){
 	int i = pos_heap[e];
-	this->heap[i] = nkey;
+	heap[i] = nkey;
 	if(heap[i] < heap[parent(i)]){ //key is smaller than parent's
 		heapify_up(e);
-	}else{
+	}else{ //check if respects propoperties w.r.t. children
 		heapify_down(e);
 	}
 }
 
+//Sifts an item e up, when necessary
 void NHeap::heapify_up(int e){
 	int i = pos_heap[e];
 	if(i == 0)
 		return;
-	if(heap[i] < heap[parent(i)]){
-		printf("Sensing %i up\n", data[i]);
+	
+	if(heap[i] < heap[parent(i)]){ //key is smaller than parent's
 		//swap key
 		int temp = heap[i];
 		heap[i] = heap[parent(i)];
 		heap[parent(i)] = temp;
+		
 		//swap data
+		int dpar = data[parent(i)];
 		temp = data[i];
 		data[i] = data[parent(i)];
 		data[parent(i)] = temp;
 		
-		pos_heap[data[i]] = parent(i);
-		pos_heap[data[parent(i)]] = i;
-		
+		//swap position pointer
+		pos_heap[e] = parent(i);
+		pos_heap[dpar] = i;
+
 		heapify_up(data[parent(i)]);
 	}
 }
 
+//Sifts an item e down, when necessary
 void NHeap::heapify_down(int e){
 	int i = pos_heap[e];
 	int min = i;
+	
 	//get smallest child
 	for(int j=first_child(i);j<heap.size();j++){
 		if(heap[min] > heap[j])
 			min = j;
 	}
-	
-	if(min == i)
+	if(min == i) //e is not smaller than any of its children
 		return;	
 	
 	//swap key
 	int temp = heap[min];
 	heap[min] = heap[i];
 	heap[i] = temp;
+	
 	//swap data
+	int dmin = data[min];
 	temp = data[min];
 	data[min] = data[i];
 	data[i] = temp;
 	
-	pos_heap[data[min]] = i;
-	pos_heap[data[i]] = min;
+	//swap position pointer
+	pos_heap[e] = min;
+	pos_heap[dmin] = i;
 	
 	heapify_down(data[min]);
 }	
 
+//Return the minimum element
 int NHeap::getmin(){
 	return data[0];
 }
 
+//Removes the top-most element, i.e., the minimum
 void NHeap::deletemin(){
 	if(heap.size() > 1){
 		pos_heap.erase(data[0]);
 		heap[0] = heap.back();
 		data[0] = data.back();
+		heap.pop_back();
+		data.pop_back();
 		pos_heap[data[0]] = 0;
+		heapify_down(data[0]);
 	}else if(heap.size() == 1){
 		pos_heap.erase(data[0]);
 		heap.pop_back();
 		data.pop_back();
 	}
 }
-
+//Return the parent of node i
 int NHeap::parent(int i){
 	return floor((i-1)/n);
 }
 
+//Return first child of node i
 int NHeap::first_child(int i){
 	return n*i+1;
 }
 
+
+void NHeap::print(){
+
+	for(int i=0;i<heap.size();i++){
+		printf("%i - %i\n",i, heap[i]);
+	}
+
+}
--- dijkstra/nheap.h
+++ dijkstra/nheap.h
@@ -1,24 +1,37 @@
-
+#ifndef __NHEAP_H__
+#define __NHEAP_H__
 #include <vector>
 #include <map>
 
 class NHeap{
 private:
-	std::vector<int> heap;
-	std::vector<int> data;
-	std::map<int, int> pos_heap;  	
-	int n;
+	std::vector<int> heap; //heap itself, ordering the keys
+	std::vector<int> data; //auxilary vector containing the data of each key
+	std::map<int, int> pos_heap; //a map to point where in the heap each item is 	
+	int n; //heap dimension : n-heap
 
+	//Sifts an item e up, when necessary
 	void heapify_up(int key);
+	//Sifts an item e down, when necessary
 	void heapify_down(int key);
+	//Return the parent of node i
 	int parent(int i);
+	//Return first child of node i
 	int first_child(int i);
 
 public:
+	//Create a new n-heap with an unique item (key, e)
 	NHeap(int n, int key, int e);
+	//Insert a new item in the heap (key, e)
 	void insert(int key, int e);
-	void update(int nkey, int e);
+	//Update the key of an item e
+	void update_key(int nkey, int e);
+	//Removes the top-most element, i.e., the minimum
 	void deletemin();
+	//Return the minimum element
 	int getmin();
+	void print();
 
 };
+
+#endif //__NHEAP_H__
