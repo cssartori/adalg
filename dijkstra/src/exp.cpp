94a22110fce2149bd65ffdba545512d4f6650926
cssartori <carlosartori2@gmail.com>
2017-04-11 20:50:40 -0300

    Generalizing experiments and adding refined results

--- dijkstra/src/exp.cpp
+++ dijkstra/src/exp.cpp
@@ -11,27 +11,29 @@ unsigned int NUM_EXP = 20;
 using namespace std;
 
 vector<unsigned int> swaps;
-vector<double> times;
+vector<long double> times;
 vector<unsigned int> e;
 
 std::chrono::time_point<std::chrono::system_clock> tstart, tend;
-std::chrono::duration<double> elapsed_seconds;
+std::chrono::duration<long double> elapsed_seconds;
 
 void read_parameters(int argc, char **argv, char *op, unsigned int *hd);
 void usage(char **argv);
 
 void test_delete(unsigned int hd){
-	NHeap h(hd, pow(2, NUM_EXP)+1);
+	NHeap h(hd, pow(hd, NUM_EXP)+1);
 	
 	for(unsigned int i=1;i<=NUM_EXP;i++){
-		int N = pow(2, i) - 1;
+		int N = ((pow(hd,i)-1)/(hd-1));
 		int n = N;
 		int k=0;
 		while(n > 0){
 			n--;
 			h.insert(rand()%N, n);
 		}
-
+        
+        n=0;
+        N = pow(hd, i-1);
 	   	tstart = std::chrono::system_clock::now();
 		h.n_swaps = 0;
 		while(n < N){
@@ -43,30 +45,32 @@ void test_delete(unsigned int hd){
 		elapsed_seconds = tend-tstart;	
 		
 		swaps[i] = h.n_swaps;
+		e[i] = i*pow(hd,i);
 		times[i] = elapsed_seconds.count();
 	}
 	
-	printf("%i,%u,%u,%lf\n", 0, swaps[0], e[0], times[0]);	
+	printf("%i %u %u %Le %Le\n", 0, swaps[0], e[0], times[0], (long double)0.0);
 	for(unsigned int j=1;j<swaps.size();j++){
-		printf("%i,%u,%u,%lf\n", j, swaps[j], e[j], times[j]-times[j-1]);
+		printf("%i %u %u %Le %Le\n", j, swaps[j], e[j], times[j]-times[j-1], (times[j]-times[j-1])/e[j]);
 	}	
 }
 
 void test_update(unsigned int hd){
 	
 	for(unsigned int i=1;i<=NUM_EXP;i++){
-		unsigned int n = pow(2, i) - 1;
-		unsigned int k = pow(2, i) + 1;
+		unsigned int n = ((pow(hd,i)-1)/(hd-1));//pow(hd, i) - 1;
+		unsigned int k = ((pow(hd,i)-1)/(hd-1))*hd+1;//pow(hd, i) + 1;
 		unsigned int el = 0;
 		
-		NHeap h(hd, n*2+1);
+		NHeap h(hd, n*hd+1);
 				
-		for(unsigned int x=0;x<n;x++)
+		for(unsigned int x=0;x<n;x++){
 			h.insert(k, el++);
+		}
 		
-		n = n+1;
+		n = pow(hd, i);
 		k = k+1;
-		
+
 		vector<unsigned int> els;
 		for(unsigned int x=0;x<n;x++){
 			h.insert(k, el);
@@ -75,8 +79,9 @@ void test_update(unsigned int hd){
 		}
 		
 		
+		//update
 		h.n_swaps = 0;
-		k = k-2;
+		k = n;
 
 	   	tstart = std::chrono::system_clock::now();	
 		
@@ -89,13 +94,13 @@ void test_update(unsigned int hd){
 		
 		swaps[i] = h.n_swaps;
 		times[i] = elapsed_seconds.count();
-		e[i] = (i)*pow(2, i);
+		e[i] = (i)*pow(hd, i);
 	}
 
-	printf("%i,%u,%u,%lf\n", 0, swaps[0], e[0], times[0]);	
+	printf("%i %u %u %Le %Le\n", 0, swaps[0], e[0], times[0], (long double)0.0);	
 	for(unsigned int j=1;j<swaps.size();j++){
-		printf("%i,%u,%u,%lf\n", j, swaps[j], e[j], times[j]-times[j-1]);
-	}	
+		printf("%i %u %u %Le %Le\n", j, swaps[j], e[j], times[j]-times[j-1], (times[j]-times[j-1])/e[j]);
+	}		
 }
 
 
@@ -111,7 +116,7 @@ void test_insert(unsigned int hd){
 	unsigned int ninserts = 1;
 
    	tstart = std::chrono::system_clock::now();
-   	unsigned int NI = pow(hd,i)-1;
+   	unsigned int NI = ((pow(hd,i)-1)/(hd-1));
    	
 	for(;n > 0 && i < NUM_EXP+1;n--){
 		h.insert(n, n);
@@ -123,14 +128,15 @@ void test_insert(unsigned int hd){
 			times[i] = elapsed_seconds.count();
 			e[i] = (i-1)*pow(hd,i-1);
 			i++;
-			NI = ((NI+1)*hd)-1;
+			NI = ((pow(hd,i)-1)/(hd-1));
 			h.n_swaps = 0;
 		}
 	}
 	
-	printf("%i,%u,%u,%lf\n", 0, swaps[0], e[0], times[0]);	
-	for(unsigned int j=1;j<swaps.size();j++){
-		printf("%i,%u,%u,%lf\n", j, swaps[j], e[j], times[j]-times[j-1]);
+	printf("%i %u %u %Le %Le\n", 0, swaps[0], e[0], times[0], (long double)0.0);
+	printf("%i %u %u %Le %Le\n", 1, swaps[1], e[1], times[1], (long double)0.0);	
+	for(unsigned int j=2;j<swaps.size();j++){
+		printf("%i %u %u %Le %Le\n", j, swaps[j], e[j], times[j]-times[j-1], (times[j]-times[j-1])/e[j]);
 	}	
 }
 
@@ -156,7 +162,6 @@ void test_scale(unsigned int hd){
     			t = rand()%n;
 			tstart = std::chrono::system_clock::now();
 		    d = dijkstra_nheap_test(g, s, t, &n_ins, &n_del, &n_upd, &mu, hd);
-		   // fprintf(stderr, "generating new pair. %i -> %i = %u\n", s, t, d);
 		}while(d == MAX_DIST);
 		
 		tend = std::chrono::system_clock::now();
@@ -166,7 +171,7 @@ void test_scale(unsigned int hd){
 		if(NUM_EXP != 1)
 			printf("%u,", i);
 				
-		printf("%u,%u,%u,%u,%u,%lu,%f,%u\n", n, m, n_ins, n_del, n_upd, mu, elapsed_seconds.count(), d);
+		printf("%u,%u,%u,%u,%u,%lu,%Le,%u\n", n, m, n_ins, n_del, n_upd, mu, elapsed_seconds.count(), d);
 		if(d == MAX_DIST)
 			ninf++;
 			
@@ -221,7 +226,7 @@ int main(int argc, char **argv){
 	read_parameters(argc, argv, &op, &hd);
 
 	swaps = vector<unsigned int>(NUM_EXP+1, 0);
-	times = vector<double>(NUM_EXP+1, 0.0);
+	times = vector<long double>(NUM_EXP+1, 0.0);
 	e = vector<unsigned int>(NUM_EXP+1, 0);
 
 	if(op == 'i')
--- dijkstra/tabler.py
+++ dijkstra/tabler.py
@@ -6,8 +6,10 @@ def __prepareargs__():
     parser = argparse.ArgumentParser(description='Creates result tables for N-Heap Dijkstra implementation')
     parser.add_argument('-d', nargs=1, type=str, help='directory with results', required=True)
     parser.add_argument('-o', nargs=1, type=str, help='output file name', required=True)
+    parser.add_argument('-x', nargs=1, type=int, help='experiment identifier (insert, update, delete=1, comp1=2, comp2=3, scale=4)', required=True)
     parser.add_argument('-g', nargs=1, type=str, help='output file for graphic plotting', required=False, default = None)
     parser.add_argument('-e', nargs=1, type=str, help='result files extension (default = \'.dat\')', required=False, default = ['.dat'])
+
         
     return parser
 
@@ -15,25 +17,42 @@ def __prepareargs__():
 def __getargs__(parser):
     args = vars(parser.parse_args())
     return args
-    
 
-#The main module, in case this program was called as the main program
-if __name__ == '__main__':
-    #receive and prepare the arguments
-    parser = __prepareargs__()
-    args = __getargs__(parser)
-    
-    #read parameters
-    dirname  = args['d'][0]
-    outfname = args['o'][0]
-    gfname = None
-    if(args['g'] != None):
-        gfname   = args['g'][0]
-    rfext    = args['e'][0]
-    
-    
+def __proc_exp1__(dirname, outfname, rfext):
     lr = []
-    
+    outf = open(outfname, "w")
+    print("reading files") 
+    for file in [f for f in os.listdir(dirname) if f.endswith(rfext)]:
+        print("Reading file "+file)
+        with open(dirname+file, 'rb') as csvfile:
+            csvdata = csv.reader(csvfile, delimiter=',')
+
+            for row in csvdata:
+                i = int(row[0])
+                nswaps = int(row[1])
+                ne = int(row[2])
+                time = float(row[3])  
+                
+                tpe = 0.0
+                if ne != 0:
+                    tpe = time/ne
+                
+                l = [i, nswaps, ne, time, tpe]           
+                lr.append(l)
+
+	
+	    lr = sorted(lr, key=itemgetter(0))
+	    outf.write("----"+dirname+file+"----\n")    
+        for l in lr:
+            outf.write("%i\t%i\t%i\t%f\t%f\n" % (l[0], l[1], l[2], l[3], l[4]))
+
+   	    outf.write("------------------------\n")
+
+
+
+def __proc_exp23__(dirname, outfname, rfext):
+    lr = []
+    outf = open(outfname, "w")
     for file in [f for f in os.listdir(dirname) if f.endswith(rfext)]:
         with open(dirname+file, 'rb') as csvfile:
             csvdata = csv.reader(csvfile, delimiter=',')
@@ -69,7 +88,35 @@ if __name__ == '__main__':
     outf = open(outfname, "w")
     
     for l in lr:
-        outf.write("%i\t%i\t%f\t%f\t%f\t%f\t%f\n" % (l[0], l[1], l[2], l[3], l[4], (l[5]/(1024*1024)), l[6]))
+        outf.write("%i\t%i\t%f\t%f\t%f\t%f\t%Le\n" % (l[0], l[1], l[2], l[3], l[4], (l[5]/(1024*1024)), l[6]))
+   
+
+
+
+
+#The main module, in case this program was called as the main program
+if __name__ == '__main__':
+    #receive and prepare the arguments
+    parser = __prepareargs__()
+    args = __getargs__(parser)
+    
+    #read parameters
+    dirname  = args['d'][0]
+    outfname = args['o'][0]
+    exp      = int(args['x'][0])
+    
+    gfname = None
+    if(args['g'] != None):
+        gfname   = args['g'][0]
+    rfext    = args['e'][0]
+    
+    if exp == 1:
+        __proc_exp1__(dirname, outfname, rfext)    
+    elif exp == 2 or exp == 3:
+        __proc_exp23__(dirname, outfname, rfext)
+
+    
+   
     
 
     
