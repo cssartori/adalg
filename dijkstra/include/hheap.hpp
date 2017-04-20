dc6dd471713440ad231105452904d49f5cd03b2a
cssartori <carlosartori2@gmail.com>
2017-04-20 13:55:47 -0300

    Hollow insertion and delete working. Update needs tests

--- dijkstra/include/hheap.hpp
+++ dijkstra/include/hheap.hpp
@@ -24,7 +24,11 @@ public:
 
     void insert(unsigned int *data, unsigned int key){
         Node *n = make_heap(data, key);
-        this->h = meld(n, this->h);
+//        if(n == nullptr)
+//            printf("Inserting null pointer\n");
+//        else
+//            printf("n = (%u,%u) | n = %u | ns = %u | fc = %u\n", *n->item->data, n->key, n, n->ns, n->fc);
+        this->h = meld(this->h, n);
         this->pos_h[*data] = n->item;
         //A new node has been added to the heap
         this->ne += 1;
@@ -32,6 +36,8 @@ public:
     };
     
     unsigned int* getmin(){
+        if(this->h == nullptr || this->h->item == nullptr)
+            return nullptr;
         return this->h->item->data;
     };
     
@@ -65,6 +71,10 @@ public:
         unsigned int M = ceil(log(this->nt)/log(1.6180));
         this->h = remove_min(this->h, M);
     };  
+
+    bool is_empty(){
+        return this->h == nullptr;
+    }
 
     unsigned int getnt(){
         return this->nt;
@@ -125,7 +135,9 @@ private:
     };
 
     Node* make_heap(unsigned int *data, unsigned int key){
-        return new Node(data, key, nullptr, this->h, (unsigned int)0);
+        Node *n = new Node(data, key, nullptr, this->h, (unsigned int)0);
+        n->ns = n;
+        return n;
     };
     
     Node* meld(Node *h1, Node *h2){
@@ -135,11 +147,13 @@ private:
             return h1;
 
         Node *temp = h1->ns;
-        printf("temp = %u\n", temp);
+       // printf("temp = %u\n", temp);
         h1->ns = h2->ns;
-        printf("-temp = %u\n", temp);
+       // printf("-temp = %u\n", temp);
         h2->ns = temp;
-
+        
+       // printf ("(%u,%u) - > (%u,%u)\n", *h2->item->data, h2->key, *h2->ns->item->data, h2->ns->key);
+        
         if(h1->key <= h2->key)
             return h1;
         else
@@ -170,40 +184,31 @@ private:
         
         std::vector<Node*> R(M, nullptr);
         Node *r = hr;
-        printf("Starting link_heap loop\n");
+      //  printf("Starting link_heap loop\n");
         do{
-            printf("*rn = r->ns\n");
-            if(r == nullptr)
-                printf("r is null\n");
             Node *rn = r->ns;
-            printf("link_heap()\n");
             link_heap(r, &R);
-            printf("r = rn\n");
             r = rn;
-            printf("Next loop\n");
         }while(r != hr);
         
-        //Rebuild heap
-        unsigned int i = 0;
-        //get first not-null reference           
-        while(i <= M && R[i] == nullptr) i++;
-        if(i > M) return nullptr;
+      //  printf("Finished loop\n");
         
-        hr = R[i];
-        hr->ns = hr;
-
-        while(i <= M){
-            R[i]->ns = R[i];
-            hr = meld(hr, R[i]);
-            i++;
+        //Rebuild heap              
+        hr = nullptr;
+      //  printf("Startging last loop\n");
+        for(unsigned i=0;i<M;i++){
+            if(R[i] != nullptr){
+                R[i]->ns = R[i];
+                hr = meld(hr, R[i]);
+            }            
         }
-        
+       // printf("Returning %u\n", hr);
         return hr;
     };
     
     //link heap removing hollow nodes
     void link_heap(Node *hr, std::vector<Node*> *R){
-        printf("Link_heap\n");
+      //  printf("Link_heap\n");
         if(hr->item == nullptr){
             Node *r = hr->fc;
             while(r != nullptr){ //go through the list of childs of h
@@ -218,7 +223,7 @@ private:
             unsigned int i = hr->rank;
             //go over each reference in R
             while((*R)[i] != nullptr){
-                link(hr, (*R)[i]); //link the reference to the heap h
+                hr = link(hr, (*R)[i]); //link the reference to the heap h
                 (*R)[i] = nullptr;
                 i++;
             }
--- dijkstra/t.cpp
+++ dijkstra/t.cpp
@@ -11,15 +11,21 @@ int main(){
         h.insert(u, 100-i);
     }
     
+//    unsigned int *u = (unsigned*)malloc(sizeof(unsigned)*3);
+//    u[0] = 0; u[1] = 1; u[2] = 2;
+//    h.insert(&u[0],0);
+//    h.insert(&u[1],1);
+//    h.insert(&u[2],2);
+//    
     printf("nt = %u\nne = %u\n", h.getnt(), h.getne());
     
-    h.print_roots();
-//    
-//    for(int i=0;i<100;i++){
-//        printf("%u\n", *h.getmin());
-//        h.delete_min();
-//    }
+   // h.print_roots();
     
+    while(!h.is_empty()){
+        printf("%u\n", *h.getmin());
+        h.delete_min();
+    }
+    printf("nt = %u\nne = %u\n", h.getnt(), h.getne());
     printf("Done.\n");
 
     return 0;
