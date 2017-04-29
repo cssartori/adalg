96513fc0e3a1dbbf67a5c9f201a093f4218c36e8
Carlo Sartori <carlo@ifrit>
2017-04-28 22:40:59 -0300

    Updating runner

--- dijkstra/run_scale.sh
+++ dijkstra/run_scale.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# Define a timestamp function                                                                                                                                           
+timestamp() {
+  date +"%Y-%m-%d :: %H:%M:%S"
+}
+
+
 mkdir "scale"
 
 drh="scale/hollow"
@@ -11,14 +17,18 @@ mkdir $drk
 for filename in ../../instances/*.gr
 	do
 		o=$(echo $filename| cut -d'/' -f 4)
+		echo "==============================================="
         echo "Running instance "$o
-	   	o=$(echo $outf| cut -d'.' -f  2)
+	timestamp
+	   	o=$(echo $o| cut -d'.' -f  2)
 	   	ork=$drk"/"$o"-K.dat"
         orh=$drh"/"$o"-H.dat"
 		./exp -t s -h k -n 50 < $filename > $ork		
 		echo "Finished k-heap. Results in "$ork
+		timestamp
     	./exp -t s -h h -n 50 < $filename > $orh
     	echo "Finished hollow. Results in "$orh
+	timestamp
 	   	echo "Done with "$filename
 done
 
