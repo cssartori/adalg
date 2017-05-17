23f78e37ffe209109be11ba87607a4e23fa381e4
cssartori <carlosartori2@gmail.com>
2017-05-16 23:45:57 -0300

    Updated generator script

--- maxflow/instances/gen.sh
+++ maxflow/instances/gen.sh
@@ -2,22 +2,20 @@ gcc -O3 new_washington.c -o new_washington 2> /dev/null
 
 mkdir "exp"
 
-for s in {50,100,150,200,250,300,350,400,450,500}
+for s in {50,100,200,300,400,500,600,700,800}
 do
     ./new_washington 1 $s $s $s exp/it_1_$s.gr
-done
-echo "Done 1"
-for s in {50,100,150,200,250,300,350,400,450,500}
-do
     ./new_washington 2 $s $s $s exp/it_2_$s.gr
-done
-echo "Done 2"
-for s in {50,100,150,200,250,300,350,400,450,500}
-do
     ./new_washington 3 $s $s $s exp/it_3_$s.gr
+    ./new_washington 4 $s $s $s exp/it_4_$s.gr
 done
-echo "Done 3"
-#for s in {50,100,150,200,250,300,350,400,450,500}
-#do
-#    ./new_washington 5 $s $s $s exp/it_5_$s.gr
-#done
+echo "Done types 1,2,3,4"
+
+for s in {50,100,200,300,400}
+do
+    ./new_washington 5 $s $s $s exp/it_5_$s.gr
+    ./new_washington 6 $s $s $s exp/it_6_$s.gr
+    ./new_washington 7 $s $s $s exp/it_7_$s.gr
+    ./new_washington 8 $s $s $s exp/it_8_$s.gr
+done
+echo "Done types 5,6,7,8"
