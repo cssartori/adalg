62be97eefc06e8b55b3e6424fec343e6eb423599
cssartori <carlosartori2@gmail.com>
2017-05-18 23:51:47 -0300

    Finished instance script generator

--- maxflow/instances/gen.sh
+++ maxflow/instances/gen.sh
@@ -2,27 +2,4 @@ gcc -O3 new_washington.c -o new_washington 2> /dev/null
 
 mkdir "exp"
 
-for s in {50,100,200,300,400,500,600,700,800}
-do
-    ./new_washington 1 $s $s $s exp/it_1_$s.gr
-    ./new_washington 2 $s $s $s exp/it_2_$s.gr
-    ./new_washington 3 $s $s $s exp/it_3_$s.gr
-    ./new_washington 4 $s $s $s exp/it_4_$s.gr
-done
-echo "Done types 1,2,3,4"
-
-for s in {50,100,200,300,400,500,600,800}
-do
-    ./new_washington 5 $s 8 $s exp/it_5_$s.gr
-    ./new_washington 6 $s $s 8 $s exp/it_6_$s.gr
-    ./new_washington 7 $s $s 8 $s exp/it_7_$s.gr
-    ./new_washington 8 $s $s 8 $s exp/it_8_$s.gr
-done
-echo "Done types 5,6,7,8"
-
-for s in {32768,65536,131072,262144}
-do
-    ./new_washington 9 $s exp/it_9_$s.gr
-    ./new_washington 10 $s exp/it_10_$s.gr
-done
-echo "Done types 9,10"
+python generate.py
--- maxflow/instances/generate.py
+++ maxflow/instances/generate.py
@@ -1,9 +1,9 @@
 import os
 
 dirname="exp"
-
-for i in range(6,12):
-    for j in range(6,12):
+count = 0
+for i in range(6,11):
+    for j in range(6,11):
         if i+j <= 18:  #maximum sum of exponents so that run time is not > 300 s
             r = 2**i
             c = 2**j
@@ -15,4 +15,93 @@ for i in range(6,12):
                 par3 = ("./new_washington 3 %i %i %i %s/gm3_r%i_c%i_k%i.gr" % (r,c,k,dirname,r,c,k))    
                 os.system(par3)
                 print "Finished [1,2,3]: %i %i %i" % (r,c,k)
+                count += 3
+                
 
+print "Finished %i types 1, 2, 3" % (count)
+print "========================================"
+
+count = 0
+for i in range(6,12):
+    for j in range(6,i+1):
+        if i+j <= 22:
+            r = 2**i
+            c = 2**j
+            for k in [100, 10000]:
+                par4 = ("./new_washington 4 %i %i %i %s/gm4_r%i_c%i_k%i.gr" % (r,c,k,dirname,r,c,k))
+                os.system(par4)
+                print "Finished [4]: %i %i %i" % (r,c,k)
+                count += 1
+                
+print "Finished %i type 4" % (count)
+print "========================================"
+
+count = 0
+for i in range(6,10):
+    for j in range(3,5):
+        d = 2**i
+        D = 2**j
+        for k in [100,10000]:
+            par5 = ("./new_washington 5 %i %i %i %s/gm5_d%i_D%i_k%i.gr" % (d,D,k,dirname,d,D,k))
+            os.system(par5)
+            print "Finished [5]: %i %i %i" % (d,D,k)
+            count += 1
+            
+print "Finished %i type 5" % (count)
+print "========================================"
+
+count = 0
+for i in range(6,9):
+    for j in range(6,9):
+        n = 2**i
+        m = 2**j
+        for d in range(3,5):
+            D = 2**d
+            for k in [100,10000]:
+                par6 = ("./new_washington 6 %i %i %i %i %s/gm6_n%i_m%i_D%i_k%i.gr" % (n,m,D,k,dirname,n,m,D,k))
+                os.system(par6)
+                print "Finished [6]: %i %i %i %i" % (n,m,D,k)
+                count += 1
+            
+print "Finished %i type 6" % (count)
+print "========================================"
+
+count = 0
+for i in range(6,10):
+    for j in range(6,10):
+        n = 2**i
+        m = 2**j
+        for d in range(3,5):
+            D = 2**d
+            for k in [100,10000]:
+                par7 = ("./new_washington 7 %i %i %i %i %s/gm7_n%i_m%i_D%i_k%i.gr" % (n,m,D,k,dirname,n,m,D,k))
+                os.system(par7)
+                par8 = ("./new_washington 8 %i %i %i %i %s/gm8_n%i_m%i_D%i_k%i.gr" % (n,m,D,k,dirname,n,m,D,k))
+                os.system(par8)
+                print "Finished [7,8]: %i %i %i %i" % (n,m,D,k)
+                count += 2
+            
+print "Finished %i types 7,8" % (count)
+print "========================================"
+            
+count = 0
+for i in range(14,25):
+    n = 2**i
+    par9 = ("./new_washington 9 %i %s/gm9_n%i.gr" % (n,dirname,n))
+    os.system(par9)
+    print "Finished [9]: %i" % (n)
+    count += 1
+
+print "Finished %i types 9" % (count)
+print "========================================"
+
+count = 0
+for i in range(6,16):
+    n = 2**i
+    par10 = ("./new_washington 10 %i %s/gm10_n%i.gr" % (n,dirname,n))
+    os.system(par10)
+    print "Finished [10]: %i" % (n)
+    count += 1
+
+print "Finished %i types 10" % (count)
+print "========================================"
