efb0bfdd6ec642d580b67f2cee26d68a69609edf
cssartori <carlosartori2@gmail.com>
2017-05-18 23:53:20 -0300

    Updated generate to run on server

--- maxflow/instances/generate.py
+++ maxflow/instances/generate.py
@@ -1,40 +1,40 @@
 import os
 
 dirname="exp"
-count = 0
-for i in range(6,11):
-    for j in range(6,11):
-        if i+j <= 18:  #maximum sum of exponents so that run time is not > 300 s
-            r = 2**i
-            c = 2**j
-            for k in [100,10000]:
-                par1 = ("./new_washington 1 %i %i %i %s/gm1_r%i_c%i_k%i.gr" % (r,c,k,dirname,r,c,k))
-                os.system(par1)
-                par2 = ("./new_washington 2 %i %i %i %s/gm2_r%i_c%i_k%i.gr" % (r,c,k,dirname,r,c,k))
-                os.system(par2)
-                par3 = ("./new_washington 3 %i %i %i %s/gm3_r%i_c%i_k%i.gr" % (r,c,k,dirname,r,c,k))    
-                os.system(par3)
-                print "Finished [1,2,3]: %i %i %i" % (r,c,k)
-                count += 3
-                
+#count = 0
+#for i in range(6,11):
+#    for j in range(6,11):
+#        if i+j <= 18:  #maximum sum of exponents so that run time is not > 300 s
+#            r = 2**i
+#            c = 2**j
+#            for k in [100,10000]:
+#                par1 = ("./new_washington 1 %i %i %i %s/gm1_r%i_c%i_k%i.gr" % (r,c,k,dirname,r,c,k))
+#                os.system(par1)
+#                par2 = ("./new_washington 2 %i %i %i %s/gm2_r%i_c%i_k%i.gr" % (r,c,k,dirname,r,c,k))
+#                os.system(par2)
+#                par3 = ("./new_washington 3 %i %i %i %s/gm3_r%i_c%i_k%i.gr" % (r,c,k,dirname,r,c,k))    
+#                os.system(par3)
+#                print "Finished [1,2,3]: %i %i %i" % (r,c,k)
+#                count += 3
+#                
 
-print "Finished %i types 1, 2, 3" % (count)
-print "========================================"
+#print "Finished %i types 1, 2, 3" % (count)
+#print "========================================"
 
-count = 0
-for i in range(6,12):
-    for j in range(6,i+1):
-        if i+j <= 22:
-            r = 2**i
-            c = 2**j
-            for k in [100, 10000]:
-                par4 = ("./new_washington 4 %i %i %i %s/gm4_r%i_c%i_k%i.gr" % (r,c,k,dirname,r,c,k))
-                os.system(par4)
-                print "Finished [4]: %i %i %i" % (r,c,k)
-                count += 1
-                
-print "Finished %i type 4" % (count)
-print "========================================"
+#count = 0
+#for i in range(6,12):
+#    for j in range(6,i+1):
+#        if i+j <= 22:
+#            r = 2**i
+#            c = 2**j
+#            for k in [100, 10000]:
+#                par4 = ("./new_washington 4 %i %i %i %s/gm4_r%i_c%i_k%i.gr" % (r,c,k,dirname,r,c,k))
+#                os.system(par4)
+#                print "Finished [4]: %i %i %i" % (r,c,k)
+#                count += 1
+#                
+#print "Finished %i type 4" % (count)
+#print "========================================"
 
 count = 0
 for i in range(6,10):
@@ -95,13 +95,13 @@ for i in range(14,25):
 print "Finished %i types 9" % (count)
 print "========================================"
 
-count = 0
-for i in range(6,16):
-    n = 2**i
-    par10 = ("./new_washington 10 %i %s/gm10_n%i.gr" % (n,dirname,n))
-    os.system(par10)
-    print "Finished [10]: %i" % (n)
-    count += 1
+#count = 0
+#for i in range(6,16):
+#    n = 2**i
+#    par10 = ("./new_washington 10 %i %s/gm10_n%i.gr" % (n,dirname,n))
+#    os.system(par10)
+#    print "Finished [10]: %i" % (n)
+#    count += 1
 
-print "Finished %i types 10" % (count)
-print "========================================"
+#print "Finished %i types 10" % (count)
+#print "========================================"
