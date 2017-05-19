50498ed1a723fd2d74f2699c7b12ad6dfd359785
cssartori <carlosartori2@gmail.com>
2017-05-18 23:54:54 -0300

    Updated runner

--- maxflow/scripts/run_exp.sh
+++ maxflow/scripts/run_exp.sh
@@ -5,7 +5,7 @@ timestamp() {
   date +"%Y-%m-%d :: %H:%M:%S"
 }
 
-drn="res"
+drn="../res"
 mkdir $drn
 i=0
 
@@ -17,23 +17,7 @@ for filename in ../instances/exp/*.gr
 	    timestamp
 	   	f=$(echo $f| cut -d'.' -f 1)
         of=$drn"/R_t_"$f".dat"
-		./exp -k 4 -n 1 < $filename > $of		
-		echo "Finished "$i". Results in "$of
-		timestamp
-	   	echo "Done with "$filename
-	   	i=$((i + 1))
-done
-
-
-for filename in ../instances/exp/*.gr
-	do
-		f=$(echo $filename| cut -d'/' -f 4)
-		echo "==============================================="
-        echo "Running instance "$i": "$f
-	    timestamp
-	   	f=$(echo $f| cut -d'.' -f 1)
-        of=$drn"/R"$f".dat"
-		./exp -k 4 -n 10 < $filename > $of		
+		../exp -k 4 -n 10 < $filename > $of		
 		echo "Finished "$i". Results in "$of
 		timestamp
 	   	echo "Done with "$filename
