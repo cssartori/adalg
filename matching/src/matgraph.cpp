18e6d9fcb3fa6ab887797c94d56f0b07b5dac759
cssartori <carlosartori2@gmail.com>
2017-05-31 11:35:08 -0300

    Started search_path function in matching

--- matching/src/matgraph.cpp
+++ matching/src/matgraph.cpp
@@ -37,6 +37,59 @@ void read_dimacs_matching_graph(Graph& g, std::istream& in, unsigned int* n, uns
 
 }
 
+void search_paths(const Graph& g, vector<Node> mates, vector<unsigned int> v1){
+    //TODO: change to queue
+    //TODO: use vertex 0 as dummy (maybe)
+    //TODO: check how to represent tree H
+    //TODO: check how to represent matching M
+    //TODO: check how to represent path P
+    
+    vector<bool> visited(num_vertices(g), false);
+    vector<unsigned int> dist(num_vertices(g), 0);
+    vector<unsigned int> u1, u2;
+    for(unsigned int i=0;i<v1.size();i++){
+        if(mates[i] == NULL_NODE)
+            u1.push_back(i);
+    }
+    
+    bool found = false;
+    do{
+        //select neighbors in V2 throughout free edges
+        while(u1.size() > 0){
+            unsigned int u = u1.back();
+            visited[u] = true;
+            u1.pop_back();
+            
+            graph_traits<Graph>::out_edge_iterator ie, fe;  //initial edge iterator and final edge
+		    for(tie(ie, fe) = out_edges(u, g); ie != fe; ie++){
+			    unsigned int v = target(*ie, g);
+			    if(!visited[v]){
+			        dist[v] = dist[u]+1;
+			        u2.push_back(v);
+			    }
+			}
+        }
+        
+        //select neighbors in V1 throughout matched edges
+        while(u2.size() > 0){
+            unsigned int u = u2.back();
+            visited[u] = true;
+            u2.pop_back();
+            
+            if(mates[u] == NULL_NODE){ //free way found
+                found = true;
+            }else{
+                unsigned int v = mates[u];
+                if(!visited[v]){
+                    dist[v] = dist[u]+1;
+                    u1.push_back(v);
+                }
+            }
+        }
+        
+    }while(!found); 
+}
+
 unsigned int hopcroft_karp(const Graph& g){
     
     unsigned int n = num_vertices(g);
