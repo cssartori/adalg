c726a5a3e7937874d0aa20e765e13b709d5897a7
cssartori <carlosartori2@gmail.com>
2017-06-11 16:29:50 -0300

    Updating matching runner script

--- matching/scripts/run_exp.sh
+++ matching/scripts/run_exp.sh
@@ -14,16 +14,16 @@ mkdir $drn1
 mkdir $drn0
 mkdir $drn2
 
-dm1=$drn1"mat_p1/"
-dm0=$drn0"mat_p0/"
-dm2=$drn2"mat_p2/"
+dm1=$drn1"mat/"
+dm0=$drn0"mat/"
+dm2=$drn2"mat/"
 mkdir $dm1
 mkdir $dm0
 mkdir $dm2
 
-df1=$drn1"flow_p1/"
-df0=$drn0"flow_p0/"
-df2=$drn2"flow_p2/"
+df1=$drn1"flow/"
+df0=$drn0"flow/"
+df2=$drn2"flow/"
 mkdir $df1
 mkdir $df0
 mkdir $df2
@@ -48,25 +48,6 @@ for filename in ../instances/exp_p1/*.gr
 done
 
 i=0
-for filename in ../instances/exp_p2/*.gr
-	do
-		f=$(echo $filename| cut -d'/' -f 4)
-		echo "==============================================="
-        echo "Running instance "$i": "$f
-	    timestamp
-	   	f=$(echo $f| cut -d'.' -f 1)
-        of="/R_"$f".dat"
-        echo "Matching running"
-		../exp -o 1 < $filename > $dm2$of
-		echo "Flow running"
-		../exp -o 2 < $filename > $df2$of		
-		echo "Finished "$i"."
-		timestamp
-	   	echo "Done with "$filename
-	   	i=$((i + 1))
-done
-
-i=0
 for filename in ../instances/exp_p0/*.gr
 	do
 		f=$(echo $filename| cut -d'/' -f 4)
@@ -79,6 +60,25 @@ for filename in ../instances/exp_p0/*.gr
 		../exp -o 1 < $filename > $dm0$of
 		echo "Flow running"
 		../exp -o 2 < $filename > $df0$of		
+		echo "Finished "$i"."
+		timestamp
+	   	echo "Done with "$filename
+	   	i=$((i + 1))
+done
+
+i=0
+for filename in ../instances/exp_p2/*.gr
+	do
+		f=$(echo $filename| cut -d'/' -f 4)
+		echo "==============================================="
+        echo "Running instance "$i": "$f
+	    timestamp
+	   	f=$(echo $f| cut -d'.' -f 1)
+        of="/R_"$f".dat"
+        echo "Matching running"
+		../exp -o 1 < $filename > $dm2$of
+		echo "Flow running"
+		../exp -o 2 < $filename > $df2$of		
 		echo "Finished "$i"."
 		timestamp
 	   	echo "Done with "$filename
