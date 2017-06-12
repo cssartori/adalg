1cfde0d42130a1986db83a87c53b2858fa0fe8ed
cssartori <carlosartori2@gmail.com>
2017-06-12 16:17:07 -0300

    Added fattest path option on matching reduction to flow

--- matching/src/exp.cpp
+++ matching/src/exp.cpp
@@ -5,101 +5,21 @@
 #include <queue>
 #include <stack>
 #include "../include/matgraph.h"
+#include "../../heap/include/heap.h"
+#include "../../heap/include/nheap.h"
 #include <boost/graph/push_relabel_max_flow.hpp>
 #include "../../common/mem_used.hpp"
 
 using namespace std;
 using namespace boost;
 
-// graph element descriptors
-typedef adjacency_list_traits<vecS,vecS,directedS>::vertex_descriptor FlowNode;
-typedef adjacency_list_traits<vecS,vecS,directedS>::edge_descriptor FlowEdge;
- 
-// a directed graph with reverse edges
-struct VertexInformation {};
-typedef unsigned Capacity;
-struct EdgeInformation {
-  Capacity edge_capacity;
-  Capacity edge_residual_capacity;
-  FlowEdge reverse_edge;
-};
- 
-typedef adjacency_list<vecS,vecS,directedS,VertexInformation,EdgeInformation> FlowGraph;
- 
 static unsigned int NUM_EXP = 10;
 
-void read_parameters(int argc, char **argv, unsigned int *op);
-void usage(char **argv);
-
-// Read a graph in DIMACS format from an input stream and return a Graph
-void read_dimacs_flow_matching_graph(FlowGraph& g, std::istream& in, unsigned int* n, unsigned int* m, unsigned int *s, unsigned int *t) {
-
-	std::string line="", dummy;
-	while (line[0] != 'p' || line[1] != ' ' || line[2] != 'e' || line[3] != 'd' || line[4] != 'g' || line[5] != 'e'){
-        getline(in,line);
-    }
- 
-  	//get nodes and edges
-    sscanf(line.c_str(), "p edge %u %u\n", n, m);
-	for(unsigned int x=0;x<*n+2;x++)
-		add_vertex(g);
-  	
-  	*s = 0;
-  	*t = *n+1;
-  	  	
-  	unsigned i=0;
-  	while (i<*m) {
-    	getline(in,line);
-    	if (line[0] == 'e' && line[1] == ' ') {
-      		unsigned int u,v;
-      		sscanf(line.c_str(), "e %u %u\n", &u, &v);
-        	
-        	if(u > *n/2){
-        	    unsigned int temp = u;
-        	    u = v;
-        	    v = temp;
-        	}
-        		
-      		FlowEdge e1, e2;
-            bool in1, in2;
-            tie(e1, in1) = add_edge(u, v, g);
-            tie(e2, in2) = add_edge(v, u, g);
-            
-            g[e1].edge_capacity = 1;
-            g[e2].edge_capacity = 0;
-            g[e1].reverse_edge = e2;
-            g[e2].reverse_edge = e1;
-      		
-      		i++;
-    	}
-  	}
-  	
-  	for(unsigned int i=1;i<=*n/2;i++){
-  	    //connect source to all in v1
-  	    FlowEdge e1, e2; 
-        bool in1, in2;   
-  	    tie(e1, in1) = add_edge(*s, i, g);
-        tie(e2, in2) = add_edge(i, *s, g);    
-        g[e1].edge_capacity = 1;
-        g[e2].edge_capacity = 0;
-        g[e1].reverse_edge = e2;
-        g[e2].reverse_edge = e1;
-        
-        //connect all v2 to target
-        FlowEdge e3, e4;    
-  	    tie(e3, in1) = add_edge(i+(*n/2), *t, g);
-        tie(e4, in2) = add_edge(*t, i+(*n/2), g);    
-        g[e3].edge_capacity = 1;
-        g[e4].edge_capacity = 0;
-        g[e3].reverse_edge = e4;
-        g[e4].reverse_edge = e3;
-  	}
-
-} 
+//==============================================================================
+//To be used by hopcroft-karp algorithm implemented
 
 //null node: when the vertex is not matched to any other valid vertex
 static const unsigned int NULL_NODE = std::numeric_limits<unsigned int>::max();
-
 struct HTreeNode{
     bool edge_used;
     unsigned int dest; //desteny of edge in H tree backward search
@@ -126,12 +46,232 @@ struct TestData{
     long double time;
     size_t mem;
 };
+//==============================================================================
 
+//==============================================================================
+//To be used by the Max-flow reduction option
+typedef boost::adjacency_list_traits<boost::vecS, boost::vecS, boost::directedS>::vertex_descriptor FlowNode;
+typedef boost::adjacency_list_traits<boost::vecS, boost::vecS, boost::directedS>::edge_descriptor FlowEdge;
+
+//a vertex's data
+struct FlowVertexData{ };
+ 
+//an edge's data
+struct FlowEdgeData{
+    unsigned int capacity;            //capacity is only positive
+    unsigned int residual_capacity; 
+    FlowEdge reverse_edge;                //backward edge for FF algorithm (O(1) access)
+};
+ 
+//A graph is an adjacency list represented by vectors (vecS)
+typedef boost::adjacency_list<boost::vecS, boost::vecS, boost::directedS, FlowVertexData, FlowEdgeData> FlowGraph;
+
+static const unsigned int MAX_FLOW = std::numeric_limits<unsigned int>::max(); 
+//A single flow's path information
+struct FlowPath {
+    vector<FlowEdge*> path;
+    unsigned int flow;
+    bool empty;
+}; 
+
+//==============================================================================
+
+void read_parameters(int argc, char **argv, unsigned int *op);
+void usage(char **argv);
+void read_dimacs_flow_matching_graph(FlowGraph& g, std::istream& in, unsigned int* n, unsigned int* m, unsigned int *s, unsigned int *t);
+TestData hopcroft_karp_test(const Graph& g);
+unsigned int fattest_path(FlowGraph& g, unsigned int s, unsigned int t, unsigned int k);
+ 
+
+
+int main(int argc, char **argv){
+    unsigned int op;
+	read_parameters(argc, argv, &op);
+    
+    unsigned int n, m, s, t;
+    
+    if(op == 1){
+        Graph g;
+        read_dimacs_matching_graph(g, cin, &n, &m);
+        for(unsigned int i=0;i<NUM_EXP;i++){
+            TestData td = hopcroft_karp_test(g);
+            printf("%u,%u,%u,%u,%u,%lu,%.2Le,%u\n",i, n, m, td.phases, td.dfsiter, td.mem, td.time, td.matching);
+        }
+    }else if(op == 2){
+        FlowGraph g;
+        read_dimacs_flow_matching_graph(g, cin, &n,&m,&s,&t);
+        for(unsigned int i=0;i<NUM_EXP;i++){
+            size_t mem = memory_used();
+            std::chrono::time_point<std::chrono::system_clock> tstart, tend;
+            std::chrono::duration<long double> elapsed_seconds;
+            tstart = std::chrono::system_clock::now();  
+            unsigned int fat = push_relabel_max_flow(g, s, t,
+                                                get(&FlowEdgeData::capacity,g),
+                                                get(&FlowEdgeData::residual_capacity,g),
+                                                get(&FlowEdgeData::reverse_edge,g),
+                                                get(boost::vertex_index, g));
+            
+            tend = std::chrono::system_clock::now();
+	        elapsed_seconds = tend-tstart;	
+	        printf("%u,%u,%u,0,0,%lu,%.2Le,%u\n",i,n,m,mem,elapsed_seconds.count(),fat);
+	    }
+	    
+    }else if(op == 3){
+        FlowGraph g;
+        read_dimacs_flow_matching_graph(g, cin, &n,&m,&s,&t);
+        if(n > pow(2,15)){
+            fprintf(stderr, "Warning! Graph is too big for fattest path.\n");
+            exit(-1);
+        }
+        for(unsigned int i=0;i<NUM_EXP;i++){
+            size_t mem = memory_used();
+            std::chrono::time_point<std::chrono::system_clock> tstart, tend;
+            std::chrono::duration<long double> elapsed_seconds;
+            tstart = std::chrono::system_clock::now();  
+            unsigned int fat = fattest_path(g, s,t,4);
+            
+            tend = std::chrono::system_clock::now();
+	        elapsed_seconds = tend-tstart;	
+	        printf("%u,%u,%u,0,0,%lu,%.2Le,%u\n",i,n,m,mem,elapsed_seconds.count(),fat);
+	        //Must set original values of graph edges in order to work
+	        graph_traits<FlowGraph>::edge_iterator ie, fe;
+        	for ( tie(ie, fe)=edges(g); ie != fe; ie++){
+	    	    g[*ie].capacity = 1;
+	    	    g[*ie].residual_capacity = 1;
+  	        }
+	    }
+    }else{
+        printf("Option %u not known.\n", op);
+    }
+	
+	return 0;
+}
+
+void read_parameters(int argc, char **argv, unsigned int *op){
+	
+	if(argc < 3){
+	    usage(argv);
+	    exit(-1);
+	}
+	
+	bool has_op = false;
+	
+	for(int i=1;i<argc;i++){
+		if(argv[i][0] == '-'){
+			switch(argv[i][1]){
+				case 'n':
+					i++;
+					NUM_EXP = atoi(argv[i]);
+					break;
+			    case 'o':
+			        i++;
+			        *op = atoi(argv[i]);
+			        has_op = true;
+			        break;
+				default:
+					fprintf(stderr, "Unkown parameter %s \n", argv[i]);
+					usage(argv);
+					exit(-1);
+			}
+		}else{
+			fprintf(stderr, "Unkown parameter %s \n", argv[i]);
+			usage(argv);
+			exit(-1);
+		}
+	}
+	
+	if(not has_op){
+	    usage(argv);
+	    exit(-1);
+	}
+	
+}
+
+void usage(char **argv){
+	fprintf(stderr, "usage:\n%s -o <test option> [-n <number of tests>]\n\t-o test option: \t1 for hopcroft-karp 2 for push-relabel\n\t-n number of tests: \tnatural numbers (default n=10)\n", argv[0]);
+}
+
+// Computes the maximum bottleneck flow from s to t in graph g using dijkstra algorithm with max-k-heap 
+FlowPath& dijkstra_flow(FlowGraph& g, unsigned int s, unsigned int t, Heap& h, FlowPath& fp){
+	
+	vector<unsigned int> fat(num_vertices(g), 0);   //vector with fattest path values
+    vector<unsigned int> visited(num_vertices(g), false); //vector of visited nodes
+    
+	fat[s] = MAX_FLOW;
+    h.insert(s, fat[s]);
+    
+    fp.empty = false;
+
+	while(!h.is_empty()){
+		unsigned int v = h.gettop(); h.deletetop();
+       	visited[v] = true;
+		graph_traits<FlowGraph>::out_edge_iterator ie, fe;  //initial edge iterator and final edge
+		for(tie(ie, fe) = out_edges(v, g); ie != fe; ie++){
+			unsigned int u = target(*ie, g);
+            if(!visited[u]){
+                if(fat[u] == 0){
+                    fat[u] = min(fat[v], g[*ie].residual_capacity);
+                    h.insert(u,fat[u]);
+                    fp.path[u] = &g[g[*ie].reverse_edge].reverse_edge;
+                }else if(fat[u] < min(fat[v], g[*ie].residual_capacity)){			    
+			        fat[u] = min(fat[v], g[*ie].residual_capacity);
+			        h.update_key(u, fat[u]);
+			        fp.path[u] = &g[g[*ie].reverse_edge].reverse_edge;	  
+			    }
+			}
+		}
+		if(v == t){
+		    while(!h.is_empty())
+		        h.deletetop();		        
+		    break;
+		}
+	}
+
+	if(fat[t] == 0) //no positive flow in the graph to reach t
+	   fp.empty = true;
+	   
+    fp.flow = fat[t];	
+    
+	return fp;
+}
+
+
+//Computes the max-flow between nodes s-t in graph g
+unsigned int fattest_path(FlowGraph& g, unsigned int s, unsigned int t, unsigned int k){
+    unsigned int flow = 0;
+    
+    NHeap h(num_vertices(g)+1, k, Heap::MAXHEAP);
+    
+    FlowPath fp;
+    fp.path = std::vector<FlowEdge*>(num_vertices(g));
+    
+    fp = dijkstra_flow(g, s, t, h, fp);
+
+    while(!fp.empty){ //while there is a path between s and t with positive flow
+       // printf("Flow of %u : %u\n", fp.flow, flow);
+        flow += fp.flow;
+        unsigned int v = t;
+
+        while(v != s){ //update the edge's in path with new flow            
+            FlowEdge e = *fp.path[v];
+            FlowEdge er = g[e].reverse_edge;
+                      
+            g[e].residual_capacity -= fp.flow;
+            g[er].residual_capacity += fp.flow;
+
+           v = source(e, g); //get previous in path
+        }
+                    
+        //get next path
+        fp = dijkstra_flow(g, s, t, h, fp);   
+    }
+        
+    return flow;
+}
 
 //BFS
 bool search_paths_test(const Graph& g, const vector<unsigned int>& v1, vector<HTreeNode>& h, const vector<Edge>& pe, Matching& mat){    
     vector<bool> visited(num_vertices(g), false);
-    //vector<unsigned int> dist(num_vertices(g), 0);
     std::queue<unsigned int> u1, u2;
     
     //get free nodes in v1 into queue u1
@@ -139,7 +279,6 @@ bool search_paths_test(const Graph& g, const vector<unsigned int>& v1, vector<HT
         if(mat.m[v1[i]] == NULL_NODE)
             u1.push(v1[i]);
     }
-    
     
     bool found = false;
     do{
@@ -198,7 +337,7 @@ bool extract_paths_test(const Graph& g, const vector<unsigned int>& v2, vector<H
     vector<bool> visited(num_vertices(g), false);
     unsigned int mp = mat.card;
     
-    //for each free vertex in v2 run a DFS : TODO: check complexity, should be O(m+n)
+    //for each free vertex in v2 run a DFS
     for(unsigned int i=0;i<v2.size();i++){
         if(mat.m[v2[i]] != NULL_NODE) continue;
         
@@ -275,6 +414,7 @@ bool extract_paths_test(const Graph& g, const vector<unsigned int>& v2, vector<H
     return false;
 }
 
+
 TestData hopcroft_karp_test(const Graph& g){
     TestData td;
     
@@ -322,85 +462,76 @@ TestData hopcroft_karp_test(const Graph& g){
     return td;
 }
 
+// Read a graph in DIMACS format from an input stream and return a Graph
+void read_dimacs_flow_matching_graph(FlowGraph& g, std::istream& in, unsigned int* n, unsigned int* m, unsigned int *s, unsigned int *t) {
 
-int main(int argc, char **argv){
-    unsigned int op;
-	read_parameters(argc, argv, &op);
-    
-    unsigned int n, m, s, t;
-    
-    if(op == 1){
-        Graph g;
-        read_dimacs_matching_graph(g, cin, &n, &m);
-        for(unsigned int i=0;i<NUM_EXP;i++){
-            TestData td = hopcroft_karp_test(g);
-            printf("%u,%u,%u,%u,%u,%lu,%.2Le,%u\n",i, n, m, td.phases, td.dfsiter, td.mem, td.time, td.matching);
-        }
-    }else{
-        FlowGraph g;
-        read_dimacs_flow_matching_graph(g, cin, &n,&m,&s,&t);
-        for(unsigned int i=0;i<NUM_EXP;i++){
-            size_t mem = memory_used();
-            std::chrono::time_point<std::chrono::system_clock> tstart, tend;
-            std::chrono::duration<long double> elapsed_seconds;
-            tstart = std::chrono::system_clock::now();  
-            unsigned int fat = push_relabel_max_flow(g, s, t,
-                                                get(&EdgeInformation::edge_capacity,g),
-                                                get(&EdgeInformation::edge_residual_capacity,g),
-                                                get(&EdgeInformation::reverse_edge,g),
-                                                get(boost::vertex_index, g));
-            
-            tend = std::chrono::system_clock::now();
-	        elapsed_seconds = tend-tstart;	
-	        printf("%u,%u,%u,0,0,%lu,%.2Le,%u\n",i,n,m,mem,elapsed_seconds.count(),fat);
-	    }
-	    
+	std::string line="", dummy;
+	while (line[0] != 'p' || line[1] != ' ' || line[2] != 'e' || line[3] != 'd' || line[4] != 'g' || line[5] != 'e'){
+        getline(in,line);
     }
-	
-	return 0;
-}
+ 
+  	//get nodes and edges
+    sscanf(line.c_str(), "p edge %u %u\n", n, m);
+	for(unsigned int x=0;x<*n+2;x++)
+		add_vertex(g);
+  	
+  	*s = 0;
+  	*t = *n+1;
+  	  	
+  	unsigned i=0;
+  	while (i<*m) {
+    	getline(in,line);
+    	if (line[0] == 'e' && line[1] == ' ') {
+      		unsigned int u,v;
+      		sscanf(line.c_str(), "e %u %u\n", &u, &v);
+        	
+        	if(u > *n/2){
+        	    unsigned int temp = u;
+        	    u = v;
+        	    v = temp;
+        	}
+        		
+      		FlowEdge e1, e2;
+            bool in1, in2;
+            tie(e1, in1) = add_edge(u, v, g);
+            tie(e2, in2) = add_edge(v, u, g);
+            
+            g[e1].capacity = 1;
+            g[e1].residual_capacity = 1;
+            g[e2].capacity = 1;
+            g[e2].residual_capacity = 1;
+            g[e1].reverse_edge = e2;
+            g[e2].reverse_edge = e1;
+      		
+      		i++;
+    	}
+  	}
+  	
+  	for(unsigned int i=1;i<=*n/2;i++){
+  	    //connect source to all in v1
+  	    FlowEdge e1, e2; 
+        bool in1, in2;   
+  	    tie(e1, in1) = add_edge(*s, i, g);
+        tie(e2, in2) = add_edge(i, *s, g);    
+        g[e1].capacity = 1;
+        g[e1].residual_capacity = 1;
+        g[e2].capacity = 1;
+        g[e2].residual_capacity = 1;
+        g[e1].reverse_edge = e2;
+        g[e2].reverse_edge = e1;
+        
+        //connect all v2 to target
+        FlowEdge e3, e4;    
+  	    tie(e3, in1) = add_edge(i+(*n/2), *t, g);
+        tie(e4, in2) = add_edge(*t, i+(*n/2), g);    
+        g[e3].capacity = 1;
+        g[e3].residual_capacity = 1;
+        g[e4].capacity = 1;
+        g[e4].residual_capacity = 1;
+        g[e3].reverse_edge = e4;
+        g[e4].reverse_edge = e3;
+  	}
 
-void read_parameters(int argc, char **argv, unsigned int *op){
-	
-	if(argc < 3){
-	    usage(argv);
-	    exit(-1);
-	}
-	
-	bool has_op = false;
-	
-	for(int i=1;i<argc;i++){
-		if(argv[i][0] == '-'){
-			switch(argv[i][1]){
-				case 'n':
-					i++;
-					NUM_EXP = atoi(argv[i]);
-					break;
-			    case 'o':
-			        i++;
-			        *op = atoi(argv[i]);
-			        has_op = true;
-			        break;
-				default:
-					fprintf(stderr, "Unkown parameter %s \n", argv[i]);
-					usage(argv);
-					exit(-1);
-			}
-		}else{
-			fprintf(stderr, "Unkown parameter %s \n", argv[i]);
-			usage(argv);
-			exit(-1);
-		}
-	}
-	
-	if(not has_op){
-	    usage(argv);
-	    exit(-1);
-	}
-	
-}
+} 
 
-void usage(char **argv){
-	fprintf(stderr, "usage:\n%s -o <test option> [-n <number of tests>]\n\t-o test option: \t1 for hopcroft-karp 2 for push-relabel\n\t-n number of tests: \tnatural numbers (default n=10)\n", argv[0]);
-}
 
