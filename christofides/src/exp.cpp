8fb6f8b21442325142fb31b0dca071c0af019d1f
cssartori <carlosartori2@gmail.com>
2017-06-28 12:48:08 -0300

    Chris: added exp module

---
+++ christofides/src/exp.cpp
@@ -0,0 +1,159 @@
+#include <cstdio>
+#include <chrono>
+#include <math.h>
+#include <fstream>
+#include <map>
+#include <vector>
+#include "../include/chrisgraph.h"
+
+using namespace std;
+using namespace Christofides;
+
+static unsigned int NUM_EXP = 10;
+static const unsigned int DEFAULT_OPMAT = BLOSSOM_MAT_ALG; //binary k-heap (2-heap)
+static unsigned int seed; //random seed
+char* bfn; //BKS file name
+
+void read_parameters(int argc, char **argv, unsigned int *opmat);
+map<string,Distance> read_bks_file();
+void usage(char **argv);
+
+
+void test(map<string,Distance> BKS, int opmat){
+
+    std::chrono::time_point<std::chrono::system_clock> start, end;
+	std::chrono::duration<double> elapsed_seconds;	
+	
+	ChrisGraph g;
+	if(read_instance(g, std::cin) != 0){
+        fprintf(stderr, "Error reading input file\n");
+        exit(-1);
+    }
+	
+	for(unsigned int n=0;n<NUM_EXP;n++){
+    	start = std::chrono::system_clock::now();
+	
+	    Distance d = run_christofides(g, opmat);
+	
+	    end = std::chrono::system_clock::now();
+        elapsed_seconds = end-start;
+        
+        Distance b = BKS[g.name];
+        double dev = double(double(d-b)/b);
+        
+        bool cappr = true; //correct approximation
+        if(dev > 0.5){
+            cappr = false;
+            if(opmat != BLOSSOM_MAT_ALG)
+                fprintf(stderr, "Approximation is wrong : %f\n", dev);   
+        }
+        
+        printf("%s,%u,%u,%u,%le,%u,%.5f,%u\n", g.name.c_str(), n, opmat, g.dim, elapsed_seconds.count(), d, dev,cappr);
+    }
+        	
+}
+
+
+int main(int argc, char **argv){
+	unsigned int opmat; //matching algorithm option
+	read_parameters(argc, argv, &opmat);
+	srand(seed);
+    
+    map<string,Distance> BKS = read_bks_file();
+    
+    test(BKS, opmat);
+	
+	return 0;
+}
+
+map<string,Distance> read_bks_file(){
+    map<string,Distance> BKS;
+
+	ifstream bfile (bfn);
+    if(bfile.is_open()){
+        string line;
+        while(getline (bfile,line)){
+            if(line[0] == '#')
+                continue;
+            string iname="", ivalue="";
+            unsigned int i;
+            for(i=0;i<line.size();i++)
+                if(line[i] == ',') break;
+                else iname += line[i];
+           
+            for(i+=1;i<line.size();i++)
+                ivalue += line[i];
+           
+            Distance value = atoi(ivalue.c_str());
+            
+            BKS[iname] = value;             
+        }
+        bfile.close();
+    }else{
+	    fprintf(stderr, "Could not open BKS file %s\n", bfn);
+	    exit(-1);
+	}
+	
+	return BKS;
+} 
+
+void read_parameters(int argc, char **argv, unsigned int *opmat){
+    if(argc < 3){
+        usage(argv);
+        exit(-1);
+    }
+    
+    bool has_file = false;
+	seed = time(0);
+	*opmat = DEFAULT_OPMAT;   	
+	
+	for(int i=1;i<argc;i++){
+		if(argv[i][0] == '-'){
+			switch(argv[i][1]){
+				case 'f':
+					i++;
+					bfn = argv[i];
+					has_file = true;    
+					break;
+			    case 'm':
+			        i++;
+	                if(argv[i][0] == 'b')
+	                    *opmat = BLOSSOM_MAT_ALG;
+	                else if(argv[i][0] == 'g')
+	                    *opmat = GREEDY_MAT_ALG;
+	                else{
+	                    fprintf(stderr, "Unkown matching algorithm %c \n", argv[i][0]);
+	                    usage(argv);
+	                    exit(-1);
+	                }
+	                break;
+				case 'n':
+					i++;
+					NUM_EXP = atoi(argv[i]);
+					break;
+			    case 's':
+			        i++;
+			        seed = atoi(argv[i]);
+			        break;
+				default:
+					fprintf(stderr, "Unkown parameter %s \n", argv[i]);
+					usage(argv);
+					exit(-1);
+			}
+		}else{
+			fprintf(stderr, "Unkown parameter %s \n", argv[i]);
+			usage(argv);
+			exit(-1);
+		}
+	}
+	
+	if(!has_file){
+	    fprintf(stderr, "A BKS file must be informed.\n");
+	    usage(argv);
+	    exit(-1);
+	}
+}
+
+void usage(char **argv){
+	fprintf(stderr, "usage:\n%s -f <bks file name> [-m <matching algorithm>] [-n <number of tests>] [-s <random seed>]\n\t-bks file name: \tpath to csv text file containing BKS values to instances\n\t-matching algorithm: \tb for blossom V, g for greedy (default b)\n\t-number of tests: \tnatural numbers (default n=10)\n\t-random seed: \t\tseed to be used by generator (default s=time(0))\n", argv[0]);
+}
